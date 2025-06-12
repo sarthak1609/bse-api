39745b95406aa262edd086fed6132ffa5633b0b8
sarthak1609 <sarthaksuryavanshi16@gmail.com>
2025-06-13 01:24:19 +0530

    fd\l

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def fetch_stock_data():
                     if i % 100 == 0:
                         print(f"✅ Fetched {i} stocks so far...")
                 except Exception as e:
-                    print(f"❌ Error fetching {code}: {e}",flush=True)
+                    print(f"❌ Error fetching {code}: {e}")
             last_updated = datetime.now().isoformat()
             print(f"✅ Finished fetching {len(all_stock_data)} stocks.")
         except Exception as e:
